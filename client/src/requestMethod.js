03b3aa727d766adb4f311d5e4426f358ecb2c222
MosTaFa-Abdulrahman <darshmostafa932@gmail.com>
2025-09-11 00:39:47 +0300

    react-version

--- client/src/requestMethod.js
+++ client/src/requestMethod.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api/";
+// const BASE_URL = "http://localhost:8080/api/";
+const BASE_URL = "https://daily-tasks-api.vercel.app/api/";
 
 const makeRequest = axios.create({
   baseURL: BASE_URL,
